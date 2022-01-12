58de2935efded9294b589c6b00b44c6be9e7ff00
nguyendung230699 <87347123+nguyendung230699@users.noreply.github.com>
2022-01-12 11:39:15 +0700

    feature:productdetail_Done

--- src/main/java/com/example/fashionstore_system/controller/ProductController.java
+++ src/main/java/com/example/fashionstore_system/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.fashionstore_system.controller;
 import com.example.fashionstore_system.entity.Product;
 import com.example.fashionstore_system.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,8 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @RequestMapping("/product")
-@RestController
+
+@Controller
 public class ProductController {
     @Autowired
     private ProductService productService;
@@ -37,9 +39,10 @@ public class ProductController {
 //
 //        return mav;
 //    }
-    @GetMapping("/Productdetail")
-    public String showProductDetail(Model model) {
-        Product product = productService.getProduct(1);
+    @GetMapping("/Productdetail/{id}")
+    public String showProductDetail(Model model, @PathVariable(name = "id") int id) {
+
+        Product product = productService.getProduct(id);
         model.addAttribute("product", product);
 
         return "product-details";
