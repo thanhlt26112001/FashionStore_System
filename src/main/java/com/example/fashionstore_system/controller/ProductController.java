3621815b03e03e50c56474adc2a2a615c186a59d
Hoàng Tuấn Anh <86347462+husterHTA@users.noreply.github.com>
2022-01-13 15:37:25 +0700

    Revert "update project"
    This reverts commit be3819b62e7a7533e6f7463e516d877e17df5528, reversing
    changes made to f370df026f29a8302f8cb4c338e4ce428988c366.

--- src/main/java/com/example/fashionstore_system/controller/ProductController.java
+++ src/main/java/com/example/fashionstore_system/controller/ProductController.java
@@ -3,50 +3,44 @@ package com.example.fashionstore_system.controller;
 import com.example.fashionstore_system.entity.Product;
 import com.example.fashionstore_system.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.domain.Sort;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
 import java.util.List;
 import java.util.Optional;
 
-//@RequestMapping("/product")
+@RequestMapping("/product")
+
 //@RestController
+
 @Controller
 public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/")
-    public String viewHomePage(Model model) {
-
-        return findPaginated(1, "id", "asc", model);
-    }
-
-
-    @GetMapping("/page/{pageNo}")
-    public String findPaginated(@PathVariable (value = "pageNo") int pageNo,
-                                @RequestParam("sortField") String sortField,
-                                @RequestParam("sortDir") String sortDir,
-                                Model model) {
-        int pageSize = 6;
-
-        Page<Product> page = productService.findPaginated(pageNo, pageSize, sortField, sortDir);
-        List<Product> listproduct = page.getContent();
-        model.addAttribute("currentPage", pageNo);
-        model.addAttribute("totalPages", page.getTotalPages());
-        model.addAttribute("totalItems", page.getTotalElements());
-        model.addAttribute("sortField", sortField);
-        model.addAttribute("sortDir", sortDir);
-        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+//    @GetMapping("/list")
+//    public List<Product> getAllProduct() {
+//        return productService.getAllProduct();
+//    }
+//
+//    @GetMapping("/sort")
+//    public List<Product> sortAllProduct() {
+//
+//        return productService.sortAllProduct();
+//    }
+    @GetMapping("/list")
+    public String getAllProduct(Model model, @RequestParam("field")Optional<String> field) {
+        Sort sort = Sort.by(Sort.Direction.DESC, field.orElse("price"));
+        List<Product> listproduct = productService.findAll(sort);
         model.addAttribute("listproduct", listproduct);
+
         return "shop";
     }
-
 
     @RequestMapping("/")
     public String viewHomePage(Model model, @RequestParam ("keyword") String keyword) {
@@ -64,17 +58,7 @@ public class ProductController {
 //        mav.addObject("product", product);
 //
 //        return mav;
-
-    
-//    @GetMapping("/list/page")
-//    public String paginate(Model model, @RequestParam("p") Optional<Integer> p){
-//        Pageable pageable = PageRequest.of(p.orElse(0),6);
-//        Page<Product> page = productService.findAll(pageable);
-//        model.addAttribute("listproduct", page);
-//        return "shop";
-
 //    }
-
     @GetMapping("/Productdetail/{id}")
     public String showProductDetail(Model model, @PathVariable(name = "id") int id) {
 
--- src/main/java/com/example/fashionstore_system/repository/ProductRepository.java
+++ src/main/java/com/example/fashionstore_system/repository/ProductRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+    List<Product> findAllByOrderByPriceDesc();
 
     //search for products by name
     @Query("SELECT p. From Product p WHERE p.name lIKE %?1%")
